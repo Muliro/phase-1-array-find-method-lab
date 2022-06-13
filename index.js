90072224ad5f01dd86f5989425b95791c76297d2
michael muliro <michael.muliro@student.moringaschool.com>
2022-06-13 13:55:07 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+function superbowlWin(list){   //this functions searches for a superbowl win year, returns undefined of no win is found
+    const findWin = list.find(greatYear => greatYear.result === "W");
+    if(findWin !== undefined){
+        return findWin.year;
+    }
+    else{
+        return findWin;
+    }
+}
